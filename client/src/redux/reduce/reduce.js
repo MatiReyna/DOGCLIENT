598f562979ea3d72d5ccc09321a8bfe57d48f8b1
Matias Reyna <matiireyna@hotmail.com>
2024-08-06 20:52:55 -0300

    Creacion del reduce y configuracion

---
+++ client/src/redux/reduce/reduce.js
@@ -0,0 +1,5 @@
+const initialState = {};
+
+const reduce = (state = initialState, action) => {};
+
+export default reduce;
--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
+// <-- Importamos el reduce -->
+import reduce from '../reduce/reduce';
+
 const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
+    reduce,
     composeEnhacer(applyMiddleware(thunkMiddleware))
 );
 
