825e8656e7a357e50624b5aa0038562880eb591f
Matias Reyna <matiireyna@hotmail.com>
2024-08-06 20:48:32 -0300

    Creacion de la primera action

---
+++ client/src/redux/actions/actions.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { GET_ALL_DOGS } from './actions-types';
+
+export const getAllDogs = () => {
+    return async function (dispatch) {
+        try {
+            const response = await axios.get('http://localhost:3001/dogs');
+            const dogs = response.data;
+            dispatch({
+                type: GET_ALL_DOGS,
+                payload: dogs
+            });
+        } catch (error) {
+            console.log('Error fetching dogs:', error)
+        }
+    }
+};
