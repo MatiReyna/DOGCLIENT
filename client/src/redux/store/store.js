9a74c38ee6e1ba8d04c9e6b2649a0c3362381767
Matias Reyna <matiireyna@hotmail.com>
2024-08-06 20:32:53 -0300

    Configuracion y creacion del store

---
+++ client/src/redux/store/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    composeEnhacer(applyMiddleware(thunkMiddleware))
+);
+
+export default store;
