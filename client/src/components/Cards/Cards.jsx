465ba6e1fc7d6903989d62cc727bfa3daae1b17c
Matias Reyna <matiireyna@hotmail.com>
2024-08-08 20:26:54 -0300

    Armando las cards de los perros

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -3,7 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Cards = ({ dog }) => {
     return (
-        <div></div>
+        <div>
+            <img src={dog.image} alt={dog.name} />
+            <div>
+                <h2>{dog.name}</h2>
+                <p>{dog.temperaments ? dog.temperaments.join(', ') : 'N/A'}</p>
+            </div>
+        </div>
     )
 };
 
