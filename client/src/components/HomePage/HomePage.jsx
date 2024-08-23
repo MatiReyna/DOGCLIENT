a6331271e12e0f75c5d60fa2640214126e212085
Matias Reyna <matiireyna@hotmail.com>
2024-08-23 20:27:10 -0300

    Renderizado correctamente de perros

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllDogs } from '../../redux/actions/actions';
 
@@ -9,11 +9,26 @@ const HomePage = () => {
 
     const dispatch = useDispatch();
 
+    const [ loading, setLoading ] = useState(true);
+    const [ error, setError ] = useState(null);
+
     const dogs = useSelector((state) => state.dogs);  // Obtiene la lista de perros del estado global.
 
     useEffect(() => {
-        dispatch(getAllDogs());
-    }, []);
+        const fetchDogs = async () => {
+            try {
+                await dispatch(getAllDogs());
+                setLoading(false);
+            } catch (error) {
+                setError('Failed to fetch dogs');
+                setLoading(false);
+            }
+        };
+        fetchDogs();
+    }, [dispatch]);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>{error}</p>;
 
     return (
         <div>
--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -4,7 +4,7 @@ import { GET_ALL_DOGS } from './actions-types';
 export const getAllDogs = () => {
     return async function (dispatch) {
         try {
-            const response = await axios.get('http://localhost:3001/dogs');
+            const response = await axios.get('http://localhost:3001/dog');
             const dogs = response.data;
             dispatch({
                 type: GET_ALL_DOGS,
