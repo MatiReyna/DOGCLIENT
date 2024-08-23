0f2cb65b403ab0462b389bd14820716f1c9d4645
Matias Reyna <matiireyna@hotmail.com>
2024-08-23 20:48:04 -0300

    Nueva action para buscar por nombre

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_ALL_DOGS } from './actions-types';
+import { GET_ALL_DOGS, GET_DOG_BY_NAME } from './actions-types';
 
 export const getAllDogs = () => {
     return async function (dispatch) {
@@ -12,6 +12,21 @@ export const getAllDogs = () => {
             });
         } catch (error) {
             console.log('Error fetching dogs:', error)
+        }
+    }
+};
+
+export const getDogByName = (name) => {
+    return async function (dispatch) {
+        try {
+            const response = await axios.get(`http://localhost:3001/dogs?name=${name}`);
+            const dogs = response.data;
+            dispatch({
+                type: GET_DOG_BY_NAME,
+                payload: dogs
+            });
+        } catch (error) {
+            console.log(`Error fetching dogs by name "${name}":`, error);
         }
     }
 };
