606c20121d2cc92a7964fe1bde579920a68acf5c
Matias Reyna <matiireyna@hotmail.com>
2024-08-26 21:36:51 -0300

    Logica para el componente Pagination

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 
-const Pagination = () => {};
+const Pagination = ({ dogsPerPage, totalDogs, paginate }) => {
+
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalDogs / dogsPerPage); i++) {
+        pageNumbers.push(i)
+    };
+
+    return (
+        <nav>
+            <ul>
+                {
+                    pageNumbers.map(number => (
+                        <li key={number}>
+                            <button onClick={ () => paginate(number) }>
+                                {number}
+                            </button>
+                        </li>
+                    ))
+                }
+            </ul>
+        </nav>
+    )
+};
 
 export default Pagination;
